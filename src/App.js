00081bb0f22ac0f56b0b41f3d49be374c6d64a0f
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-11-06 18:18:41 +0100

    add style to TodoCounter

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <TodoSearch />
       <TodoList>
         {todos.map((todo) => (
-          <TodoItem />
+          <TodoItem key={todo.text} text={todo.text} />
         ))}
       </TodoList>
       <CreateTodoButton />
