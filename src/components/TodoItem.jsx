47ef1027f01de24b87327495e768b950f2d2c134
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-11-08 20:18:16 +0100

    Add function onclick on completed task and deleted task

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,17 +2,29 @@ import React from "react";
 import "./TodoItem.css";
 
 function TodoItem(props) {
+  const onComplete = () => {
+    alert("Ya completaste la tarea " + props.text);
+  };
+
   return (
     <li className="TodoItem">
       <span
         className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+        onClick={onComplete}
       >
         √
       </span>
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete">X</span>
+      <span
+        className="Icon Icon-delete"
+        onClick={() => {
+          alert("Eliminaste la tarea " + props.text);
+        }}
+      >
+        X
+      </span>
     </li>
   );
 }
