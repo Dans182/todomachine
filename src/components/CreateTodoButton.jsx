6bf61bc1971af831bb9d537ab2713dc5d12ad085
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-11-08 20:11:47 +0100

    Add function onclick on button

--- src/components/CreateTodoButton.jsx
+++ src/components/CreateTodoButton.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import "./CreateTodoButton.css"
+import "./CreateTodoButton.css";
 
-function CreateTodoButton() {
+function CreateTodoButton(props) {
+
+  // const onClickButton = () =>{
+  //   alert("Prueba")
+  // }
+
   return (
     <>
-      <button className="CreateTodoButton">+</button>
+      <button
+        className="CreateTodoButton"
+        //hay que envolverlo nuestro codigo del onClick en una función o arrow function.
+        //Es igualmente valido crear una función fuera, definirla y despues en el onClick
+        //simplemente invocarla.
+        //Pero lo que no podemos hacer es mandar una orden, directamente, sin función en el
+        //onClick, ya que esto lo que haría es ejecutar el codigo en el primer renderizado
+        //Aca no está haciendo su "funcionalidad", no hace su función.
+        onClick={() => {
+          console.log("Click!");
+        }}
+      >
+        +
+      </button>
     </>
   );
 }
