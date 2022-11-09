e8de4247f8363c5290afb1732d88113c04f570b1
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-11-09 22:39:39 +0100

    add usestate in todosearch

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
+  const [value, setValue] = useState("");
+  //  Puedes o bien importar el useState arriba y despues invocar el useState como en la linea de arriba o
+  //puedes prescindir de importarlo, pero lo invocas como abajo "React.useState."
+  //  const [value, setValue] = React.useState("")
+
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
   };
