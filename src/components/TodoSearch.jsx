ac87a3012a0b79fa05f258dbb2d51f7eeb929ccd
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-11-10 07:49:15 +0100

    todo search fixed

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
-  const [value, setValue] = useState("");
+  const [searchValue, setSearchValue] = useState("");
   //  Puedes o bien importar el useState arriba y despues invocar el useState como en la linea de arriba o
   //puedes prescindir de importarlo, pero lo invocas como abajo "React.useState."
   //  const [value, setValue] = React.useState("")
 
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
+    setSearchValue(event.target.value)
   };
 
   return (
@@ -17,7 +18,9 @@ function TodoSearch() {
         className="TodoSearch"
         onChange={onSearchValueChange}
         placeholder="Add your task"
+        value={searchValue}
       />
+      {searchValue}
     </>
   );
 }
