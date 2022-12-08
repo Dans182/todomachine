9772ec4870bfa71629f9dab5c7f0a1fcf160547f
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-12-08 19:20:34 +0100

    ADD UseContext in TodoSearch

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
+import { TodoContext } from "../Context";
 import "../styles/components/TodoSearch.css";
 
-function TodoSearch({ searchValue, setSearchValue }) {
+function TodoSearch() {
   // const [searchValue, setSearchValue] = useState("");
   //  Puedes o bien importar el useState arriba y despues invocar el useState como en la linea de arriba o
   //puedes prescindir de importarlo, pero lo invocas como abajo "React.useState."
   //  const [value, setValue] = React.useState("")
+  const { searchValue, setSearchValue } = useContext(TodoContext);
 
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
