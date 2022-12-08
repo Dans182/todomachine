a4f61d1ffb273ca4244a6dc1636c38b21b0314ad
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-12-08 19:15:29 +0100

    ADD UseContext

--- src/pages/AppUI.jsx
+++ src/pages/AppUI.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { TodoContext } from "../Context";
 import { TodoCounter } from "../components/TodoCounter";
 import { TodoSearch } from "../components/TodoSearch";
@@ -8,35 +8,34 @@ import { CreateTodoButton } from "../components/CreateTodoButton";
 
 // Desescructuramos las nuevas props
 function AppUI() {
+  const { error, loading, searchedTodos, completeTodo, deleteTodo } =
+    useContext(TodoContext);
   return (
     <React.Fragment>
       {/* Pasamos el estado a nuestro componente */}
       <TodoCounter />
       <TodoSearch />
-      <TodoContext.Consumer>
-        {({error, loading, searchedTodos, completeTodo, deleteTodo}) => (
-          <TodoList>
-            {error && <p>Desespérate, hubo un error...</p>}
-            {/* Mostramos un mensaje de cargando, cuando la aplicación está cargando
+
+      <TodoList>
+        {error && <p>Desespérate, hubo un error...</p>}
+        {/* Mostramos un mensaje de cargando, cuando la aplicación está cargando
         lo sdatos */}
-            {loading && <p>Estamos cargando, no desesperes...</p>}
-            {/* Si terminó de cargar y no existen TODOs, se muestra un mensaje para
+        {loading && <p>Estamos cargando, no desesperes...</p>}
+        {/* Si terminó de cargar y no existen TODOs, se muestra un mensaje para
         crear el primer TODO */}
-            {!loading && !searchedTodos.length && <p>¡Crea tu primer TODO!</p>}
-            {/* Acá me despliega tantos TodosItems como haya registrados */}
-            {/* Regresamos solamente los TODOs buscados */}
-            {searchedTodos.map((todo) => (
-              <TodoItem
-                key={todo.text}
-                text={todo.text}
-                completed={todo.completed}
-                onComplete={() => completeTodo(todo.text)}
-                onDelete={() => deleteTodo(todo.text)}
-              />
-            ))}
-          </TodoList>
-        )}
-      </TodoContext.Consumer>
+        {!loading && !searchedTodos.length && <p>¡Crea tu primer TODO!</p>}
+        {/* Acá me despliega tantos TodosItems como haya registrados */}
+        {/* Regresamos solamente los TODOs buscados */}
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
       <CreateTodoButton />
     </React.Fragment>
   );
