9da8c69d3823410f48436a56e4b2d9e3dc039637
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-12-08 19:18:22 +0100

    FIX UseContext in TodoCounter

--- src/components/TodoCounter.jsx
+++ src/components/TodoCounter.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { TodoContext } from "../Context";
 import "../styles/components/TodoCounter.css";
 
 // const estilos = {
@@ -10,11 +11,11 @@ import "../styles/components/TodoCounter.css";
 function TodoCounter() {
   // function TodoCounter(props) {
   //  const {total, completed} = props;
-  const { total, completed } = useContext;
+  const { totalTodos, completedTodos } = useContext(TodoContext);
   return (
     <>
       <h2 className="TodoCounter">
-        Has completado {completed} de {total} TODOs
+        Has completado {completedTodos} de {totalTodos} TODOs
       </h2>
     </>
   );
